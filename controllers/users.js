1b80a69e4adb994482e91ee7a51fdf10633e4f31
Zoom <andrey.kropachev@lamoda.ru>
2022-07-16 01:13:31 +0300

    feat: add error

--- controllers/users.js
+++ controllers/users.js
@@ -1,22 +1,25 @@
 const User = require('../models/user');
 
+const BAD_REQUEST_ERROR = 400;
+const NOT_FOUND_ERROR = 404;
+const DEFAULT_ERROR = 500;
+
 module.exports.getUsers = (req, res) => {
   User.find({})
     .then((users) => res.send({ data: users }))
-    .catch(() => res.send({ message: 'Произошла ошибка' }));
+    .catch(() => res.status(DEFAULT_ERROR).send({ message: 'Произошла ошибка' }));
 };
 
 module.exports.getUser = (req, res) => {
   User.findById(req.params._id)
-    .then((user) => {
-      if (!user) {
-        res.send('Такого пользователя не существует');
-        return;
+    .then((user) => res.send({ data: user }))
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        res.status(BAD_REQUEST_ERROR).send({ message: 'Некорректные данные' });
+      } else {
+        res.status(DEFAULT_ERROR).send({ message: 'Произошла ошибка' });
       }
-
-      res.send({ data: user });
-    })
-    .catch(() => res.send({ message: 'Произошла ошибка' }));
+    });
 };
 
 module.exports.createUser = (req, res) => {
@@ -33,11 +36,7 @@ module.exports.setUser = (req, res) => {
 
   User.findByIdAndUpdate(userId, { name, about }, { new: true, runValidators: true })
     .then((user) => res.send({ data: user }))
-    .catch((err) => {
-      if (err.name === 'ValidationError') {
-        throw new Error(err.message);
-      }
-    });
+    .catch(() => res.send({ message: 'Произошла ошибка' }));
 };
 
 module.exports.setAvatar = (req, res) => {
