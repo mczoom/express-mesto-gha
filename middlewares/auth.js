793b3aac01858c62933bcc9038a48c650c0c846e
Zoom <andrey.kropachev@lamoda.ru>
2022-07-28 00:25:36 +0300

    feat: add carId validation

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,20 +1,21 @@
 const jwt = require('jsonwebtoken');
 const AuthoriseError = require('../errors/AuthoriseError');
 
+const { NODE_ENV, JWT_SECRET } = process.env;
+
 module.exports.auth = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    throw new AuthoriseError('Необходимо авторизоваться');
+    next(new AuthoriseError('Необходима авторизация'));
   }
   const token = String(req.headers.authorization).replace('Bearer ', '');
-
   let payload;
 
   try {
-    payload = jwt.verify(token, 'some-secret-key');
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key');
   } catch (err) {
-    throw new AuthoriseError('Необходимо авторизоваться');
+    return next(new AuthoriseError('Необходима авторизация'));
   }
   req.user = payload;
   next();
