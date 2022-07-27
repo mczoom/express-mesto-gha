77857b2714dc16f8f3aee579eadaeef8147d8716
Zoom <andrey.kropachev@lamoda.ru>
2022-07-28 00:57:44 +0300

    feat: add validation to users routes

--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,14 @@ const usersRouter = require('express').Router();
 const {
   getUsers, getUser, updateUserInfo, setAvatar, getCurrentUser,
 } = require('../controllers/users');
+const { userInfoValidation, idValidation, avatarValidation } = require('../middlewares/validation');
+const auth = require('../middlewares/auth');
 
+usersRouter.use(auth);
 usersRouter.get('/users', getUsers);
-usersRouter.get('/users/:userId', getUser);
+usersRouter.get('/users/:userId', idValidation, getUser);
 usersRouter.get('/users/me', getCurrentUser);
-usersRouter.patch('/users/me', updateUserInfo);
-usersRouter.patch('/users/me/avatar', setAvatar);
+usersRouter.patch('/users/me', userInfoValidation, updateUserInfo);
+usersRouter.patch('/users/me/avatar', avatarValidation, setAvatar);
 
 module.exports = usersRouter;
