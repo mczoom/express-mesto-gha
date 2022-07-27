38581663a103281892665eb761fc572abad567f6
Zoom <andrey.kropachev@lamoda.ru>
2022-07-27 23:59:49 +0300

    feat: add carId validation

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const usersRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
 const { login, createUser } = require('./controllers/users');
 const { loginValidation, userValidation } = require('./middlewares/validation');
-// const auth = require('./middlewares/auth');
+const auth = require('./middlewares/auth');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -30,6 +30,7 @@ app.use('*', (req, res) => {
   res.status(404).send({ message: 'Страница не найдена' });
 });
 
+app.use(auth);
 app.use(errors());
 
 app.use((err, req, res, next) => {
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,18 +1,21 @@
 const jwt = require('jsonwebtoken');
 const AuthoriseError = require('../errors/AuthoriseError');
 
-const { NODE_ENV, JWT_SECRET } = process.env;
-
 module.exports.auth = (req, res, next) => {
-  const token = req.cookies.jwt;
+  const { authorization } = req.headers;
+
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    next(new AuthoriseError('Необходимо авторизоваться'));
+  }
+  const token = String(req.headers.authorization).replace('Bearer ', '');
+
   let payload;
 
   try {
-    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'super-strong-secret');
+    payload = jwt.verify(token, 'some-secret-key');
   } catch (err) {
     throw new AuthoriseError('Необходимо авторизоваться');
   }
   req.user = payload;
-
-  next();
+  return next();
 };
