2f0a4da52b0d42c01962b7a9faea1a5dd8cf0b37
Zoom <andrey.kropachev@lamoda.ru>
2022-07-28 00:14:07 +0300

    feat: add carId validation

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const usersRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
 const { login, createUser } = require('./controllers/users');
 const { loginValidation, userValidation } = require('./middlewares/validation');
-const auth = require('./middlewares/auth');
+// const auth = require('./middlewares/auth');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -24,7 +24,7 @@ app.use(cookieParser());
 app.post('/signin', loginValidation, login);
 app.post('/signup', userValidation, createUser);
 
-app.use(auth);
+// app.use(auth);
 app.use('/', usersRouter);
 app.use('/', cardsRouter);
 app.use('*', (req, res) => {
