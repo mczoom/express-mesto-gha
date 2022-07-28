65901eb6d6cf37d88f6866da532e79363423460f
Zoom <andrey.kropachev@lamoda.ru>
2022-07-29 01:43:15 +0300

    fix: cards controller

--- controllers/cards.js
+++ controllers/cards.js
@@ -11,7 +11,7 @@ module.exports.getCards = (req, res, next) => {
 
 module.exports.createCard = (req, res, next) => {
   const { name, link } = req.body;
-  const owner = req.user;
+  const owner = req.user._id;
 
   Card.create({ name, link, owner })
     .then((card) => {
